e3cdc91ecf003f85755f0f7f32208f4bd7119121
sviveros05@gmail.com <sviveros05@gmail.com>
2019-08-13 15:49:28 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,34 @@
+//Test to seee if JS is working
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+// types of cards
+var cards = ["queen", "queen", "king", "king"];
 
-console.log("User flipped " + cardFour);
+// empty array that will have card values pushed in as used they become in play
+var cardsInPlay = [];
+
+// assining to first card value "queen"
+var cardOne = cards[0];
+
+// Pusing the value into cardsInPlay
+cardsInPlay.push(cardOne);
+
+// displaying which card is flipped to the console.
 console.log("User flipped " + cardOne);
+
+// assining to third card value "king"
+var cardTwo = cards[2];
+
+// Pusing the value into cardsInPlay
+cardsInPlay.push(cardTwo);
+
+// displaying which card is flipped to the console.
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again!")
+	}
+}
