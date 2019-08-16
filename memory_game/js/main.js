d4c47fdc05c048dc0dfede644af4c026b170afce
sviveros05@gmail.com <sviveros05@gmail.com>
2019-08-16 14:17:04 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -7,28 +7,28 @@ var cards = ["queen", "queen", "king", "king"];
 // empty array that will have card values pushed in as used they become in play
 var cardsInPlay = [];
 
-// assining to first card value "queen"
-var cardOne = cards[0];
-
-// Pusing the value into cardsInPlay
-cardsInPlay.push(cardOne);
-
-// displaying which card is flipped to the console.
-console.log("User flipped " + cardOne);
-
-// assining to third card value "king"
-var cardTwo = cards[2];
-
-// Pusing the value into cardsInPlay
-cardsInPlay.push(cardTwo);
-
-// displaying which card is flipped to the console.
-console.log("User flipped " + cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!")
-	} else {
-		alert("Sorry, try again!")
-	}
+function checkForMatch() {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		  console.log("You found a match!");
+		} else {
+		  console.log("Sorry, try again.");
+		}
+	} 
 }
+
+function flipCard(cardId) {
+	cardsInPlay.push(cardId);
+	console.log("User flipped " + cards[cardId]);
+	checkForMatch();
+	// if (cardsInPlay.length === 2) {
+	// 	if (cardsInPlay[0] === cardsInPlay[1]) {
+	// 		alert("You found a match!")
+	// 	} else {
+	// 		alert("Sorry, try again!")
+	// 	}
+	// } 
+}
+
+flipCard(0);
+flipCard(2);
